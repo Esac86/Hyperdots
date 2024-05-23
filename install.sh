3cf845a04f5d3a95d06e07028543f728aab461d1
Edwin Santiago Acevedo Forero <esacevedo86@gmail.com>
2024-05-22 22:08:54 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Variables
 CONFIG_DIR="$HOME/.config"
 REPO_CONFIG_DIR="$(dirname "$0")/config"
-PACKAGE_LIST="hyprland waybar swaybg kitty wofi nemo swappy ttf-font-awesome"  # Añade aquí los paquetes que quieras instalar
+PACKAGE_LIST="hyprland waybar swaybg kitty wofi nemo swappy ttf-font-awesome firefox nano pamixer brightnessctl"  # Añade aquí los paquetes que quieras instalar
 
 # Función para actualizar e instalar paquetes
 install_packages() {
